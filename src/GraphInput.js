33f4f01de03cf3235cc4db0b80209c2df47c947d
Peter Ambruzs <acerola1@users.noreply.github.com>
2018-12-15 14:30:15 +0100

    labels for textboxes

--- src/GraphInput.js
+++ src/GraphInput.js
@@ -13,23 +13,41 @@ const GraphInput = ({ graph, config, setGraph, setConfig }) => {
   const [value1, setValue1] = useState(JSON.stringify(graph, null, '  '));
   const [value2, setValue2] = useState(JSON.stringify(config, null, '  '));
   return (
-    <div className="child-container edit-container">
-      <textarea value={value1} onChange={e => setValue1(e.target.value)} />
-      <button
-        onClick={() =>
-          isValidJson(value1) ? setGraph(JSON.parse(value1)) : ''
-        }
-      >
-        OK
-      </button>
-      <textarea value={value2} onChange={e => setValue2(e.target.value)} />
-      <button
-        onClick={() =>
-          isValidJson(value2) ? setConfig(JSON.parse(value2)) : ''
-        }
-      >
-        OK
-      </button>
+    <div className="child-container">
+      <div className="edit-container">
+        <div className="labelled-field-container">
+          <div style={{ textAlign: 'left' }}>Graph</div>
+          <div style={{ flex: '1 0' }} className="edit-container">
+            <textarea
+              value={value1}
+              onChange={e => setValue1(e.target.value)}
+            />
+            <button
+              onClick={() =>
+                isValidJson(value1) ? setGraph(JSON.parse(value1)) : ''
+              }
+            >
+              OK
+            </button>
+          </div>
+        </div>
+        <div className="labelled-field-container">
+          <div style={{ textAlign: 'left' }}>Config</div>
+          <div style={{ flex: '1 0' }} className="edit-container">
+            <textarea
+              value={value2}
+              onChange={e => setValue2(e.target.value)}
+            />
+            <button
+              onClick={() =>
+                isValidJson(value2) ? setConfig(JSON.parse(value2)) : ''
+              }
+            >
+              OK
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
